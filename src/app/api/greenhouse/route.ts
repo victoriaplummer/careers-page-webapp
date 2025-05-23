e715634e0e0e40f387c3c3b1a057384b4308830a
Zach Plata <zach.plata@webflow.com>
2025-05-23 11:34:53 -0500

    set runtime to edge on api

--- src/app/api/greenhouse/route.ts
+++ src/app/api/greenhouse/route.ts
@@ -3,18 +3,26 @@ import { NextResponse } from "next/server";
 
 export async function GET(request: Request) {
   const { searchParams } = new URL(request.url);
-  const ghSlug = searchParams.get('ghSlug') || "webflow";
+  const ghSlug = searchParams.get("ghSlug") || "webflow";
   const url = `https://boards-api.greenhouse.io/v1/boards/${ghSlug}/departments/`;
 
   try {
     const res = await fetch(url, { next: { revalidate: 60 } }); // cache for 60s
     if (!res.ok) {
-      return NextResponse.json({ error: "Failed to fetch" }, { status: res.status });
+      return NextResponse.json(
+        { error: "Failed to fetch" },
+        { status: res.status }
+      );
     }
     const data = await res.json();
     return NextResponse.json(data);
   } catch (error) {
     console.error(error);
-    return NextResponse.json({ error: "Internal Server Error" }, { status: 500 });
+    return NextResponse.json(
+      { error: "Internal Server Error" },
+      { status: 500 }
+    );
   }
 }
+
+export const runtime = "edge";
